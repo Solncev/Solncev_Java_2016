614e4d93a4aca983a3eb30b0a7fdf393deb48cfa
Marat Solncev <marat-solncev@mail.ru>
2016-11-15 23:22:09 +0300

    [homework #1]:artifact names in all modules corrected.

--- 1.Homework/answer/src/main/java/com/solncev/Main.java
+++ 1.Homework/answer/src/main/java/com/solncev/Main.java
@@ -8,8 +8,8 @@ public class Main {
         Divider divider = new Divider();
         AnswerGenerator answerGenerator = new AnswerGenerator();
         RestCalculator restCalculator = new RestCalculator();
-        System.out.println(divider.getIntegerPart(5,2));
-        System.out.println(restCalculator.getRest(7,3));
-        System.out.println(answerGenerator.createAnswer(907,21));
+        System.out.println(divider.getIntegerPart(5, 2));
+        System.out.println(restCalculator.getRest(7, 3));
+        System.out.println(answerGenerator.createAnswer(907, 21));
     }
 }
